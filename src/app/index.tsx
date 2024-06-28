5bfc8fc8729254295c1370689899b25c73b6f465
nullsploit01 <harshaldharmik099@gmail.com>
2024-06-29 02:35:52 +0530

    move asstes and app to src

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Sup.</Text>
+    </View>
+  );
+};
+
+export default App;
