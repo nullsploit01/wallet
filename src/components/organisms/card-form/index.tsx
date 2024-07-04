55b877725e22d397b3613361dee0c30d3a0b0c1c
nullsploit01 <harshaldharmik099@gmail.com>
2024-07-05 02:50:51 +0530

    add masking for card number and date

--- src/components/organisms/card-form/index.tsx
+++ src/components/organisms/card-form/index.tsx
@@ -1,5 +1,6 @@
 import { router } from 'expo-router'
 import { useState } from 'react'
+import { formatWithMask } from 'react-native-mask-input'
 import { Button, Form, Spinner, View, XStack, YStack } from 'tamagui'
 
 import TextInput from '@/src/components/atoms/text-input/input'
@@ -8,6 +9,30 @@ import { ICardFormProps } from '@/src/types/components/organisms'
 import { ICard } from '@/src/types/models/cards'
 
 const CardForm = ({ type }: ICardFormProps) => {
+  const cardNumberMask = [
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/
+  ]
+
+  const cardValidTillDateMask = [/\d/, /\d/, '/', /\d/, /\d/]
+
   const [_status, setStatus] = useState<'off' | 'submitting' | 'submitted'>('off')
   const [_cardDetails, setCardDetails] = useState<ICard>({ type } as ICard)
 
@@ -21,7 +46,6 @@ const CardForm = ({ type }: ICardFormProps) => {
   return (
     <Form
       alignItems="center"
-      minWidth={300}
       gap="$2"
       onSubmit={() => setStatus('submitting')}
       borderWidth={1}
@@ -47,11 +71,17 @@ const CardForm = ({ type }: ICardFormProps) => {
           marginBottom="$1.5"
           width="100%"
           value={_cardDetails.number}
+          keyboardType="numeric"
           placeholder="4111 4322 5675"
           label="Card Number"
           onValueChange={(v) => {
+            const { masked } = formatWithMask({
+              text: v,
+              mask: cardNumberMask
+            })
+
             setCardDetails((prev) => {
-              return { ...prev, number: v }
+              return { ...prev, number: masked }
             })
           }}
         />
@@ -61,10 +91,16 @@ const CardForm = ({ type }: ICardFormProps) => {
               value={_cardDetails.expiry?.toString()}
               marginBottom="$1.5"
               placeholder="MM/YY"
+              keyboardType="numeric"
               label="Valid Till"
               onValueChange={(v) => {
+                const { masked } = formatWithMask({
+                  text: v,
+                  mask: cardValidTillDateMask
+                })
+
                 setCardDetails((prev) => {
-                  return { ...prev, expiry: v }
+                  return { ...prev, expiry: masked }
                 })
               }}
             />
@@ -85,7 +121,7 @@ const CardForm = ({ type }: ICardFormProps) => {
         </XStack>
         <TextInput
           marginBottom="$2.5"
-          placeholder="***"
+          placeholder="American Express"
           label="Card Name"
           value={_cardDetails.label}
           onValueChange={(v) => {
