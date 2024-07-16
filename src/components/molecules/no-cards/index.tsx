186c8a00cdb5f5024749c36e68097cb2e70b2598
nullsploit01 <harshaldharmik099@gmail.com>
2024-07-17 02:48:38 +0530

    remove redundant code

---
+++ src/components/molecules/no-cards/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'tamagui'
+
+const NoCards = () => {
+  return (
+    <View flex={1} justifyContent="center" alignItems="center">
+      <Text>Please add a new card to get started</Text>
+    </View>
+  )
+}
+
+export default NoCards
--- src/components/organisms/cards/index.tsx
+++ src/components/organisms/cards/index.tsx
@@ -1,35 +1,32 @@
-import { useEffect, useState } from 'react'
 import { ScrollView, View } from 'tamagui'
 
 import DialogBox from '@/src/components/atoms/dialog-box'
 import CardDetails from '@/src/components/molecules/card-details'
+import NoCards from '@/src/components/molecules/no-cards'
 import CardForm from '@/src/components/organisms/card-form'
 import { useCardStore } from '@/src/stores/use-cards'
 import { ICardsProps } from '@/src/types/components/organisms'
-import { CardTypes, ICard } from '@/src/types/models/cards'
+import { CardTypes } from '@/src/types/models/cards'
 
 const Cards = ({ type }: ICardsProps) => {
-  const { getCards, cards } = useCardStore()
-  const [_cards, setCards] = useState<ICard[]>([])
-
-  useEffect(() => {
-    const cards = getCards()
-    setCards(cards)
-  }, [cards])
+  const { cards } = useCardStore()
 
   const cardType = type === CardTypes.Credit ? 'Credit' : 'Debit'
 
+  const getCardsByType = () => {
+    return cards.filter((card) => card.type === type)
+  }
+
   return (
     <View flex={1} justifyContent="flex-start">
+      {!getCardsByType().length && <NoCards />}
       <ScrollView>
-        {_cards.map((card) => {
-          if (card.type === type) {
-            return (
-              <View key={card.id} margin="$3">
-                <CardDetails card={card} />
-              </View>
-            )
-          }
+        {getCardsByType().map((card) => {
+          return (
+            <View key={card.id} margin="$3">
+              <CardDetails card={card} />
+            </View>
+          )
         })}
       </ScrollView>
       <View position="absolute" bottom={0} right={0} width="30%" padding={10} margin={30}>
